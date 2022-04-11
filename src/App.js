f17ebaf3e873320859d37e4887193c5a0d192710
Lena Kononchuk <kononchuk.elena91@gmail.com>
2022-04-11 15:53:33 +0300

    create directiry for project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import { HashRouter } from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HashRouter>
+          <main className="for_page">
+              <Header />
+              <div className="pages">
+
+              </div>
+              <Footer />
+          </main>
+      </HashRouter>
   );
 }
 

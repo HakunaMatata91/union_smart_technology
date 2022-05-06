777fc4c3cad1e339cb073ec74761bf70563dbc82
Lena Kononchuk <kononchuk.elena91@gmail.com>
2022-05-06 15:58:30 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.css";
 import "./component/Header/Header.scss";
@@ -7,31 +6,27 @@ import MainContent from "./component/Pages/MainContent/MainContent";
 import Team from "./component/Pages/Team/Team";
 import Services from "./component/Pages/Services/Services";
 import Career from "./component/Pages/Career/Career";
-import {Route, BrowserRouter as Router, HashRouter} from "react-router-dom";
+import {Route,  Routes, HashRouter} from "react-router-dom";
 import Portfolio from "./component/Pages/Portfolio/Portfolio";
 import Contact from "./component/Pages/Contact/Contact";
-import {Routes} from "react-router";
 
 function App() {
   return (
       <HashRouter>
-    <main className="for_page">
-      <Routes>
-      {/*<Router>*/}
-        {/*<Header />*/}
-        <div className="pages">
-            <Route path="/" element={<MainContent />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/career" element={<Career />} />
-            <Route path="/contact" element={<Contact />} />
-
-        </div>
-      {/*</Router>*/}
-      <Footer />
-      </Routes>
-    </main>
+          <main className="for_page">
+              {/*<Header />*/}
+              <div className="pages">
+                  <Routes>
+                          <Route path="/" element={<MainContent />} />
+                          <Route path="/team" element={<Team />} />
+                          <Route path="/services" element={<Services />} />
+                          <Route path="/portfolio" element={<Portfolio />} />
+                          <Route path="/career" element={<Career />} />
+                          <Route path="/contact" element={<Contact />} />
+                  </Routes>
+              </div>
+              <Footer />
+          </main>
       </HashRouter>
   );
 }
